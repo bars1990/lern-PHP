859faaa26ff4061e6020df0004c6b373aca8a454
Bars1990 <bydevpro@gmail.com>
2022-04-17 13:33:49 +0300

    lesson4

---
+++ qs.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html lang="ru">
+<head>
+    <title> Вывод параметров командной строки </title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<?php
+echo "Данные из командной строки = {$_SERVER['QUERY_STRING']}";
+?>
+</body>
+</html>
+
